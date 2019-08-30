67bb9ff842e781c362e8c5fefe9c8cccea45f670
unknown <szayashn@gmail.com>
2019-08-29 22:21:06 -0700

    now all info loads if you lost connection

--- apps/drawing_app/consumers.py
+++ apps/drawing_app/consumers.py
@@ -1,6 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 all_users = {}
+users_draw = {}
+
 class User:
     def __init__(self, id, name, isHost):
         self.id = id
@@ -19,7 +21,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
         all_users.setdefault(self.room_name, [])
-        if len(all_users[self.room_name]) == 0: #fix it
+        users_draw.setdefault(self.room_name, [])
+        
+        if len(users_draw[self.room_name]) == 0: 
+            users_draw[self.room_name].append([])
+            users_draw[self.room_name].append([])
+            users_draw[self.room_name].append([])
+            users_draw[self.room_name].append([])
+
+        if len(all_users[self.room_name]) == 0: 
             self.current_user = User(
                 self.scope['session']['user_id'],
                 self.scope['session']['user_name'],
@@ -43,6 +53,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 self.room_group_name,
                 {
                     'type': 'users_online'
+                }
+            )
+        await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'load_previous'
                 }
             )
             
@@ -94,6 +110,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             clickY = text_data_json['clickY']
             clickDrag = text_data_json['clickDrag']
             clickColor = text_data_json['clickColor']
+            users_draw[self.room_name][0].append(clickX)
+            users_draw[self.room_name][1].append(clickY)
+            users_draw[self.room_name][2].append(clickDrag)
+            users_draw[self.room_name][3].append(clickColor)
+
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -102,6 +123,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'clickY': clickY,
                     'clickDrag': clickDrag,
                     'clickColor': clickColor
+                }
+            )
+        elif 'clear' in  text_data_json:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'clear',
                 }
             )
 
@@ -118,12 +146,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
         clickY = event['clickY']
         clickDrag = event['clickDrag']
         clickColor = event['clickColor']
-        # Send message to WebSocket
+
         await self.send(text_data=json.dumps({
             'clickX': clickX,
             'clickY': clickY,
             'clickDrag': clickDrag,
             'clickColor':clickColor
+        }))
+
+    async def load_previous(self, event):
+
+        await self.send(text_data=json.dumps({
+            'just_log_in': True,
+            'clickX': users_draw[self.room_name][0],
+            'clickY': users_draw[self.room_name][1],
+            'clickDrag': users_draw[self.room_name][2],
+            'clickColor':users_draw[self.room_name][3]
         }))
 
     async def users_online(self, event):
@@ -138,4 +176,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'message': message,
             'winner': self.current_user.__dict__
         }))
-        
+    async def clear(self, event):
+        await self.send(text_data=json.dumps({
+            'clear': True
+        }))
